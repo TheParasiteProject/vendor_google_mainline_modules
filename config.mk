e11ed2c488763e8b88a296e10770745c9381a666
Dyneteve <dyneteve@hentaios.com>
2024-03-05 19:20:10 +0000

    apex: Make this build after the dexpreopt refactor
    Also remove the QPR1 hack for devicelock as the prebuilt module sdk
    is now available.
    
    Test: Compile
    
    Change-Id: I85e56d8d019c21274698af7d6ed58c0c50a6208e
    Signed-off-by: Dyneteve <dyneteve@hentaios.com>

--- config.mk
+++ config.mk
@@ -1,8 +1,8 @@
 # Anything including updatable_apex.mk should have done so by now.
 ifneq ($(TARGET_SUPPORTS_PREBUILT_UPDATABLE_APEX), false)
 
-# FIXME -- DeviceLock ModulePrebuilt is only planned to be released for QPR2
-DISABLE_DEXPREOPT_CHECK := true
+# Setup build characteristics
+PRODUCT_INCLUDE_TAGS := com.android.mainline mainline_module_prebuilt_monthly_release
 
 ifneq ($(MAINLINE_INCLUDE_UWB_MODULE), false)
 MAINLINE_INCLUDE_UWB_MODULE := true
