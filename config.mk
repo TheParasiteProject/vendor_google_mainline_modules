6064d8a56908719cb86f7f60b5836e51055700e4
someone5678 <someone5678@users.noreply.github.com>
2024-03-29 19:27:34 +0900

    mainline_module: Remove comment about DISABLE_DEXPREOPT_CHECK
    * We might seems like keep enable this flag
    
    Change-Id: I7243aeed85ef6c957ecb0e85fb83ba23b1f3ee85

--- config.mk
+++ config.mk
@@ -1,7 +1,6 @@
 # Anything including updatable_apex.mk should have done so by now.
 ifneq ($(TARGET_SUPPORTS_PREBUILT_UPDATABLE_APEX), false)
 
-# FIXME -- DeviceLock ModulePrebuilt is only planned to be released for QPR2
 DISABLE_DEXPREOPT_CHECK := true
 
 # Setup build characteristics
