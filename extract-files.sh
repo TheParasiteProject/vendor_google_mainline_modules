27583a32c4d715fe8cb0328d5f7312fb6a023b0f
someone5678 <59456192+someone5678@users.noreply.github.com>
2025-02-02 17:15:31 +0900

    mainline_modules: Update starletMagic
    * Sync to gms changes
    
    Change-Id: I46ec5b811e98f0dfd29e475804c0fccd88ca4d5f

--- extract-files.sh
+++ extract-files.sh
@@ -70,30 +70,37 @@ function blob_fixup() {
 }
 
 function starletMagic() {
-            folder=${2/.apk/}
-            apktool -q d "$2" -o $folder -f
-            rm -rf $2 $folder/{apktool.yml,original,res/values/public.xml,unknown}
-            cp ${MY_DIR}/overlay-template.txt $folder/Android.bp
-            sed -i "s|dummy|${folder##*/}|g" $folder/Android.bp
-            find $folder -type f -name AndroidManifest.xml -exec sed -i "s|extractNativeLibs\=\"false\"|extractNativeLibs\=\"true\"|g" {} \;
-            for file in $(find $folder/res -name *xml ! -path "$folder/res/raw" ! -path "$folder/res/drawable*" ! -path "$folder/res/xml"); do
-                for tag in $(cat exclude-tag.txt); do
-                    type=$(echo $tag | cut -d: -f1)
-                    node=$(echo $tag | cut -d: -f2)
-                    xmlstarlet ed -L -d "/resources/$type[@name="\'$node\'"]" $file
-                    xmlstarlet fo -s 4 $file > $file.bak
-                    mv $file.bak $file
-                done
-                sed -i "s|\?android:\^attr-private|\@\*android\:attr|g" $file
-                sed -i "s|\@android\:color|\@\*android\:color|g" $file
-                sed -i "s|\^attr-private|attr|g" $file
-            done
-            if [ "${folder##*/}" == "SettingsGoogleOverlayPixel2022" ]; then
-                sed -i "s| android\:resourcesMap\=\"\@xml\/overlays\"||g" "$folder/AndroidManifest.xml"
-                rm -rf "$folder/res/xml"
-            fi
-}
+    folder=${2/.apk/}
 
+    apktool -q d "$2" -o $folder -f
+    rm -rf $2 $folder/{apktool.yml,original,res/values/public.xml,unknown}
+
+    cp ${MY_DIR}/overlay-template.txt $folder/Android.bp
+    sed -i "s|dummy|${folder##*/}|g" $folder/Android.bp
+
+    find $folder -type f -name AndroidManifest.xml \
+        -exec sed -i "s|extractNativeLibs\=\"false\"|extractNativeLibs\=\"true\"|g" {} \;
+
+    find $folder -type f -name AndroidManifest.xml \
+        -exec sed -i "s|android:priority\=\"1\"|android:priority\=\"2\"|g" {} \;
+
+    find $folder -type f -name AndroidManifest.xml \
+        -exec sed -i "s|auto_generated_rro_product__|pixel.overlay|g" {} \;
+
+    find $folder -type f -name AndroidManifest.xml -exec sed -i "s|extractNativeLibs\=\"false\"|extractNativeLibs\=\"true\"|g" {} \;
+    for file in $(find $folder/res -name *xml ! -path "$folder/res/raw" ! -path "$folder/res/drawable*" ! -path "$folder/res/xml"); do
+        for tag in $(cat exclude-tag.txt); do
+            type=$(echo $tag | cut -d: -f1)
+            node=$(echo $tag | cut -d: -f2)
+            xmlstarlet ed -L -d "/resources/$type[@name="\'$node\'"]" $file
+            xmlstarlet fo -s 4 $file > $file.bak
+            mv $file.bak $file
+        done
+        sed -i "s|\?android:\^attr-private|\@\*android\:attr|g" $file
+        sed -i "s|\@android\:color|\@\*android\:color|g" $file
+        sed -i "s|\^attr-private|attr|g" $file
+    done
+}
 
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
